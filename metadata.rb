4249a7d2554c52c29244e27d6d6631252622fdd6
Jeremiah Dabney <jdabney@gmail.com>
2025-02-11 12:10:31 -0900

    Bumped mariadb.

--- metadata.rb
+++ metadata.rb
@@ -3,14 +3,14 @@ maintainer       'UAF RCS'
 maintainer_email 'chef@rcs.alaska.edu'
 license          'Apache-2.0'
 description      'Common OpenStack attributes, libraries and recipes.'
-version          '21.0.6'
+version          '21.0.7'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
 depends 'etcd', '~> 7.0'
-depends 'mariadb', '~> 5.0'
+depends 'mariadb', '~> 6.2.0'
 depends 'memcached', '~> 7.0'
 depends 'selinux'
 depends 'yum-centos', '>= 3.2.0'
